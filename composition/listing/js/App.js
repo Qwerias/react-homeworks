1b9de2d227ed8f0d582fc3e59f4641f6d564fb8f
Anton Andryushin <an.andryushin@gmail.com>
2018-09-27 23:55:10 +0300

    Пятое задание

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const App = ({items}) => (
-  <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
-  </main>
+	<main>
+		{items.map(item => {
+			return <ChooseColor item={item}/>
+		})}
+	</main>
 );
+
+const ChooseColor = ({item}) => {
+	switch (item.type) {
+		case 'unisex':
+			return <Item color="black" item={item}/>;
+		case 'male':
+			return <Item color="blue" item={item}/>;
+		case 'female':
+			return <Item color="orange" item={item}/>;
+	}
+};
